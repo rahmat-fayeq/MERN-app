b012df5944b4542b325a137ab2a97eacbc361a13
Rahmat <fayeq.rahmat@gmail.com>
2024-01-28 14:50:01 +0430

    4. cleanup goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const {getGoals,setGoal,updateGoal,deleteGoal} = require('../controllers/goalController');
 
-router.get('/', getGoals);
-router.post('/', setGoal);
-router.put('/:id', updateGoal);
-router.delete('/:id', deleteGoal);
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
