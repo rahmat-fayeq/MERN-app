a9e6079478b6b6dd8ad19a566ce9971f4e6701d7
Rahmat <fayeq.rahmat@gmail.com>
2024-01-30 14:01:34 +0430

    10. handle authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asynHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../model/userModel');
 
 /**
  * @desc   Register user
@@ -7,7 +10,37 @@ const asynHandler = require('express-async-handler');
  * 
  */
 const registerUser = asynHandler(async (req,res)=>{
-    res.json({message: 'register user'});
+    const {name,email,password} = req.body;
+
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error('All fields are required');
+    }
+
+    // check if user is already registered
+    const userExist = await User.findOne({email});
+    if(userExist){
+        res.status(400);
+        throw new Error('User already registered');
+    }
+    // hash password
+    const hashedPassword = await bcrypt.hash(password,10);
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+    if(!user){
+       res.status(400);
+       throw new Error('Invalid user data');
+    }
+
+    res.status(201).json({
+        _id: user._id,
+        name:user.name,
+        email:user.email
+    });
+
 });
 /**
  * @desc   Login a user
@@ -16,7 +49,29 @@ const registerUser = asynHandler(async (req,res)=>{
  * 
  */
 const loginUser = asynHandler(async (req,res)=>{
-    res.json({message: 'login user'});
+    const {email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error('Email or Password can not be blank!');
+    }
+    // check if user exist
+    const user = await User.findOne({email});
+    if(!user){
+        res.status(404);
+        throw new Error('user does not exist, please create an account');
+    }
+    // check if email and password are valid
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.status(200).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email
+        });
+    }
+    else{
+        res.status(400);
+        throw new Error('Invalid credentials !');
+    }
 });
 /**
  * @desc   Login a user
