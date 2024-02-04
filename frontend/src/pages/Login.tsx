583d24d1fd694b957f721ff1d8b15b3efd3a9a15
Rahmat <fayeq.rahmat@gmail.com>
2024-02-04 15:04:51 +0430

    20. create login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,65 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Login
+        </h1>
+        <p>Login to Account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={handleSubmitForm}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              id="email"
+              value={email}
+              onChange={handleInputChange}
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              id="password"
+              value={password}
+              onChange={handleInputChange}
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
