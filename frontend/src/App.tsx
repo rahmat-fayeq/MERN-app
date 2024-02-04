b087839237e94fb343b8a2140f225df5bfb53097
Rahmat <fayeq.rahmat@gmail.com>
2024-02-04 14:07:58 +0430

    17. add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
