b57ee92c1a6766523814f7b612c704d55e12e012
Rahmat <fayeq.rahmat@gmail.com>
2024-02-13 14:07:51 +0430

    28. deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const {errorHandler} = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
@@ -14,6 +15,15 @@ app.use(express.urlencoded({extended:false}));
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+// serve frontend in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'../','frontend','build','index.html')));
+}else{
+    app.get('/',(req,res)=>res.send('You are in development mode, please change to production mode!'));
+}
+
 app.use(errorHandler);
 app.listen(port, () => {
     console.log('listening on port ' + port)
